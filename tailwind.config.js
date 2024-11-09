ba221076d639895ac9659351ecb4284896c4dc30
ka-amina <aminakara400@gmail.com>
2024-11-09 02:02:38 +0100

    add  multi-step form

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
         primary: "#EC505D",
         secondary: "#F6EFED",
         brown: "#CA6D18",
-        "nu-gray":{ 
-          50:"#83A4B6",
-          100:"#797979",
-
+        "nu-gray": {
+          50: "#83A4B6",
+          100: "#797979",
         },
         "nu-blue": {
           50: "#F9F9F9",
@@ -19,9 +18,13 @@ module.exports = {
           300: "#0E6CC2",
           400: "#0000FF",
         },
+        input: "#F3DCDC",
       },
       opacity: {
         secondary: "0.38",
+      },
+      width: {
+        input: "35rem",
       },
     },
   },
